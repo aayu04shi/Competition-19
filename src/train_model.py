d6d707ef44dccdbcd1aaa395bc286b3ff3c617f1
aayu04shi <aayushi636naik@gmail.com>
2026-04-16 02:07:03 +0530

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -4,16 +4,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 def train_model(df):
-    df = pd.get_dummies(df)
 
-    X = df.drop("Crime Domain", axis=1)
+    # Drop high-cardinality / useless columns
+    df = df.drop([
+        "Report Number",
+        "Date Reported",
+        "Date of Occurrence",
+        "Time of Occurrence",
+        "Crime Description",
+        "Date Case Closed"
+    ], axis=1)
+
+    # Target
     y = df["Crime Domain"]
+    X = df.drop("Crime Domain", axis=1)
 
+    # Encode only useful categorical columns
+    X = pd.get_dummies(X, drop_first=True)
+
+    # Split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+    # Train
     model = RandomForestClassifier(n_estimators=100)
     model.fit(X_train, y_train)
 
+    # Save model
     pickle.dump(model, open("models/trained_model.pkl", "wb"))
 
     return model, X_test, y_test
